435ac44675f4354d2a9c9079ce51e1c198a06621
Saif <saifrehmat270@outlook.com>
2023-10-01 17:15:03 +0500

    Completed Sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { BiHomeAlt2 } from "react-icons/bi";
 import { FiBell } from "react-icons/fi";
 import { HiOutlineDotsVertical } from "react-icons/hi";
-import { GoSearch } from "react-icons/go";
+import { AiOutlineCaretLeft } from "react-icons/ai";
+import { GoSearch, GoDot } from "react-icons/go";
 import { AiFillCaretDown } from "react-icons/ai";
 import { IoIosChatboxes } from "react-icons/io";
 
@@ -15,6 +16,7 @@ const App = () => {
       setShow(true);
     }
   }
+
   return (
     <div>
       <header className="bg-[#181717] h-[70px]">
@@ -73,6 +75,206 @@ const App = () => {
           </div>
         </nav>
       </header>
+      <div className="text-[#FFFFFF] w-[240px] h-[922px] bg-[#181717] ">
+        <div className="flex justify-between items-cente p-3">
+          <div className="text-sm">
+            <p>FOLLOWED</p>
+          </div>
+          <div className="w-[21px] h-[21px] bg-[#182222] p-0.5">
+            <AiOutlineCaretLeft />
+          </div>
+        </div>
+
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-1.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">Suspendas</p>
+              <span className="text-xs text-[#A4A4A4]">VALORANT</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">153</p>
+          </div>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-2.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">Classybeef</p>
+              <span className="text-xs text-[#A4A4A4]">FORTNITE</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">4,657</p>
+          </div>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-3.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">rDizzle707</p>
+              <span className="text-xs text-[#A4A4A4]">League of Legends</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">412</p>
+          </div>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-4.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">zhayla</p>
+              <span className="text-xs text-[#A4A4A4]">Rocket League</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">1,324</p>
+          </div>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-5.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">Palmaurian</p>
+              <span className="text-xs text-[#A4A4A4]">VALORANT</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">1,200</p>
+          </div>
+        </div>
+        <div className="p-3">
+          <p className="text-[#B40105]">SHOW MORE</p>
+        </div>
+        <div className="p-3">
+          <p>RECOMMENDED</p>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-1.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">Suspendas</p>
+              <span className="text-xs text-[#A4A4A4]">VALORANT</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">153</p>
+          </div>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-2.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">Classybeef</p>
+              <span className="text-xs text-[#A4A4A4]">FORTNITE</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">4,657</p>
+          </div>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-3.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">rDizzle707</p>
+              <span className="text-xs text-[#A4A4A4]">League of Legends</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">412</p>
+          </div>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-4.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">zhayla</p>
+              <span className="text-xs text-[#A4A4A4]">Rocket League</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">1,324</p>
+          </div>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex">
+            <div>
+              <img
+                src="./avatars/avatar-5.jpg"
+                className="w-[40px] h-[40px] rounded-lg"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">Palmaurian</p>
+              <span className="text-xs text-[#A4A4A4]">VALORANT</span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <GoDot className="text-[#B40105] bg-[#B40105] w-[10px] h-[10px] rounded-full m-1" />
+            <p className="text-[#A4A4A4] text-xs">1,200</p>
+          </div>
+        </div>
+        <div className="p-3">
+          <p className="text-[#B40105]">SHOW MORE</p>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/tmp.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { AiOutlineCaretLeft } from "react-icons/ai";
+
+import { GoDot } from "react-icons/go";
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default App;
